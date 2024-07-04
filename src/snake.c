fb481405fcd17998375d757f4df38b4f743e2074
Class Account <cs61c-aal@hive12.CS.Berkeley.EDU>
2024-07-03 23:38:35 -0700

    Commit04: snake also got reset so readding it

--- src/snake.c
+++ src/snake.c
@@ -44,26 +44,41 @@ int main(int argc, char *argv[]) {
   // Read board from file, or create default board
   if (in_filename != NULL) {
     // TODO: Load the board from in_filename
+    FILE* in=fopen(in_filename,"r");
+    state=load_board(in);
+    //fclose(in);
     // TODO: If the file doesn't exist, return -1
+    if(in==NULL){
+        return -1;
+    }
     // TODO: Then call initialize_snakes on the state you made
+    state=initialize_snakes(state);
   } else if (io_stdin) {
-    // TODO: Load the board from stdin
+    // TODO: Load the board from ;stdin
+    state=load_board(stdin);
     // TODO: Then call initialize_snakes on the state you made
+    state=initialize_snakes(state);
   } else {
     // TODO: Create default state
+    state=create_default_state();
   }
 
   // TODO: Update state. Use the deterministic_food function
   // (already implemented in snake_utils.h) to add food.
+  update_state(state,deterministic_food);
 
   // Write updated board to file or stdout
   if (out_filename != NULL) {
     // TODO: Save the board to out_filename
+    //FILE* out=fopen(out_filename,"w");
+    save_board(state,out_filename);
+    //fclose(out);
   } else {
     // TODO: Print the board to stdout
+    print_board(state,stdout);
   }
 
   // TODO: Free the state
-
+  free_state(state);
   return 0;
 }
